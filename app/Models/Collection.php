decd7aa27f02df4acfa39b1c59bf7affd154af57
ArjanSingh17 <240209768@aston.ac.uk>
2025-11-26 15:41:16 +0000

    fixed so soft delete work

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -3,12 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Collection extends Model
 {
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'collections';
+
     protected $fillable = [
-        'slug','name','description','cover_image_url','creator_name',
+        'slug',
+        'name',
+        'description',
+        'cover_image_url',
+        'creator_name',
     ];
 
-    public function nfts() { return $this->hasMany(Nft::class); }
+    public function nfts()
+    {
+        return $this->hasMany(Nft::class);
+    }
 }
