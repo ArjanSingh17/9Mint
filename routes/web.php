95a98aa4da2311b39d68bb255330b0995d0946da
Jahirul-Islam2347084327 <jahirul.islam200518@gmail.com>
2025-11-26 17:38:30 +0000

    web.php test with blade

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\Api\V1\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::get('/', function (){
+return view('welcome');
+});
