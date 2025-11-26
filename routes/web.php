1ab93e97e973f35b095e95fe35dab357abae3598
Jahirul-Islam2347084327 <jahirul.islam200518@gmail.com>
2025-11-26 21:38:08 +0000

    setup ability to display front end

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
+use Inertia\Inertia;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::get('/', function (){
-return view('welcome');
+Route::get('/', function () {
+    return view('homepage');
 });
