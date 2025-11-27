b373eb9e6374d987911d2fdc5239a31e53f2930d
Jahirul-Islam2347084327 <jahirul.islam200518@gmail.com>
2025-11-27 01:20:06 +0000

    linking all pages

---
+++ resources/views/Products.blade.php
@@ -0,0 +1,49 @@
+
+        
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/products.css') }}">
+</head>
+<body>
+  
+    <x-navbar /> 
+
+    <section id="NFT_collections">
+        <h2>Our Collections</h2>
+
+            <div class="Glossy-collection">
+                <h3>
+                    <a href="/products/Glossy-collection">Glossy Collection</a>
+                </h3>
+                <p>This collection contains Glossy Animal NFTs.</p>
+                <p>Click to find more about each individual NFT.</p>
+            
+            <div class="Glossy-Stock">
+                <p>Stock: 27</p>
+            </div>
+        </div>
+
+        <div class="Superhero-collection">
+            <h3>
+                <a href="/products/SuperheroCollection">Superhero Collection</a>
+            </h3>
+
+            <p>This collection contains Superhero NFTs.</p>
+            <p>Click to find more about each individual NFT.</p>
+
+            <div class="Superhero-Stock">
+                <p>Stock: 35</p>
+            </div>
+        </div>
+    </section>
+</div>  
+</body>
+</html>
