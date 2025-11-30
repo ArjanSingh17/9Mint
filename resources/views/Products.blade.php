69934f3e84713b6733a0fc969a0945c91d50e7be
ArjanSingh17 <240209768@aston.ac.uk>
2025-11-30 05:23:39 +0000

    feat : make sure nft are showing

--- resources/views/Products.blade.php
+++ resources/views/Products.blade.php
@@ -3,36 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Products</title>
     <link rel="stylesheet" href="{{ asset('css/products.css') }}">
 </head>
+
 <body>
 
-    <x-navbar /> 
+    <x-navbar />
 
     <section id="NFT_collections">
         <h2>Our Collections</h2>
 
-        @foreach ($collections as $collection)
-            <div class="collection-card">
-                
-                <h3>
-                    <a href="/products/{{ $collection->slug }}">
-                        {{ $collection->name }}
-                    </a>
-                </h3>
+        {{-- ========= GLOSSY COLLECTION ========= --}}
+        <div class="collection-card">
+            <img class="collection-image"
+                 src="{{ asset('images/nfts/glossy/GlossyDuckNFT.png') }}"
+                 alt="Glossy Collection">
 
-                <p>{{ $collection->description }}</p>
+            <h3>
+                <a href="{{ url('/products/glossy-collection') }}">
+                    Glossy Collection
+                </a>
+            </h3>
 
-                <div class="Stock">
-                    <p>
-                        Stock: 
-                        {{ $collection->nfts()->sum('editions_remaining') }}
-                    </p>
-                </div>
+            <p>A curated set of glossy, high-quality animal NFTs.</p>
+
+            <div class="stock-count">
+                <p>Stock: {{ \App\Models\Nft::where('collection_id', 1)->sum('editions_remaining') }}</p>
             </div>
-        @endforeach
+        </div>
+
+        {{-- ========= SUPERHERO COLLECTION ========= --}}
+        <div class="collection-card">
+            <img class="collection-image"
+                 src="{{ asset('images/nfts/superhero/IronMan.png') }}"
+                 alt="Superhero Collection">
+
+            <h3>
+                <a href="{{ url('/products/superhero-collection') }}">
+                    Superhero Collection
+                </a>
+            </h3>
+
+            <p>A powerful collection of superhero-themed NFTs.</p>
+
+            <div class="stock-count">
+                <p>Stock: {{ \App\Models\Nft::where('collection_id', 2)->sum('editions_remaining') }}</p>
+            </div>
+        </div>
 
     </section>
 
