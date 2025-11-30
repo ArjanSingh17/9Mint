d71830e0fec254e67823a5fbf85259bd0540b435
ArjanSingh17 <240209768@aston.ac.uk>
2025-11-30 04:00:11 +0000

    feat(homepage): add dynamic NFT preview images using database content

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -16,26 +16,27 @@
     </nav>
 
     <section id="Information" class="Org">
+
         <div class="image-left">
-            <img src="{{ asset('images/NFT1.png')}}" alt="Left NFT"/>
+            <img 
+                src="{{ asset($nfts[0]->image_url ?? 'images/NFT1.png') }}" 
+                alt="Left NFT"
+            />
         </div>
 
         <div class="Informationstyle">
             <h2>Need an NFT?</h2>
             <h3>We got you!</h3>
         </div>
-    <div class="image-right">
-            <img src="{{ asset('images/NFT4.png')}}" alt="right NFT"/>
+
+        <div class="image-right">
+            <img 
+                src="{{ asset($nfts[1]->image_url ?? 'images/NFT4.png') }}" 
+                alt="Right NFT"
+            />
         </div>
+
     </section>
     </div>
 </body>
 </html>
-
-
-
-
-      
-   
-
-
